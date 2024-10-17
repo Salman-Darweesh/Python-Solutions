9d1f788388ca11fc3ccb665158f566c4e78495c8
Salman-Darweesh <salman.darweesh@my.utsa.edu>
2024-10-17 12:39:25 -0500

    commit

--- Salman_Darweesh_Challenge_08.py
+++ Salman_Darweesh_Challenge_08.py
@@ -2,11 +2,22 @@
 
 
 def step_01():
-    input("Please enter something into the Morse Code Translator: "))
+    user = input("Please enter something into the Morse Code Translator: ")
+    makingAllLettersCapital = user.upper()
     
+    alphabet = [" ", ",", ".", "?", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f",
+                "g","h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "x"]
+    
+    morseCodeTranslated = [" ", "--..--", ".-.-.-","..--..", "-----", ".----", "..---", "...--", "....-", ".....", "-....",
+                           "--...", "---..", "----.", ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....","..", ".---",
+                           "-.-", ".-..", "--","-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-",
+                           "-.-", "--.."]
 
-
-
+    whereTheTranslationHappens = []
+    
+    lastStepTranslation = []
+    
+    for i in makingAllLettersCapital:
 
 
 if __name__ == "__main__":
