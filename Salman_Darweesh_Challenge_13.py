f1ca8c679bffc6966a31f49ca1e3fe2953e8185b
Salman-Darweesh <salman.darweesh@my.utsa.edu>
2024-11-19 15:12:59 -0600

    commit

---
+++ Salman_Darweesh_Challenge_13.py
@@ -0,0 +1,20 @@
+
+
+
+
+import re
+
+
+def checking_if_valid(password_given):
+    pattern = r"^[a-zA-Z0-9@#$%*!]{8,}$" #reating a regex object using the raw string
+    
+    if re.match(pattern, password_given):
+        print("Valid.")
+        
+    else:
+        print("Invalid.")
+    
+
+if __name__ in "__main__":
+    password_given = input("Enter a password to see if its valid: ")
+    checking_if_valid(password_given)
