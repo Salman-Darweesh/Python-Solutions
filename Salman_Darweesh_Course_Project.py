10e85f8e3daff548249fd3fdae57ffea87f5e710
Salman-Darweesh <salman.darweesh@my.utsa.edu>
2024-11-25 12:17:33 -0600

    commit

---
+++ Salman_Darweesh_Course_Project.py
@@ -0,0 +1,36 @@
+
+
+import random
+
+class Humanoid:
+    def __init__(self, height, weight, hairColor, eyeColor):
+        self._height = height
+        self._weight = weight
+        self._hairColor = hairColor
+        self._eyeColor = eyeColor
+        self.
+        
+        pass 
+    
+    
+    
+    
+    
+    
+def character_Creation():
+    print("\n Welcome \n")
+    print("---------")
+    print("1: Human\n2: Elf\n3: Dwarf")
+    print("_________\n")
+    character_Choice = input("Please pick a character (1, 2, 3): ")
+    height = input("Please enter a Height for your character (3ft - 7ft): ")
+    weight = input("Please enter a Weight for your character (60lbs - 300lbs): ")
+    hairColor = input("Please enter a Hair Color for your character (white, silver, gray, black, brown, green, blue, yellow, pink, red, blonde): ")
+    eyeColor = input("Please enter an Eye Color for your character (white, black, red, green, blue, brown, purple, amber): ")
+    
+    
+    
+if __name__ in "__main__":
+    character_Creation()
+    
+    pass 
