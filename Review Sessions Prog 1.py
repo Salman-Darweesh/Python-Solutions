6dd09bab2c6ec96b2a70d02ae0797a92b5fa335c
Salman-Darweesh <salman.darweesh@my.utsa.edu>
2024-12-03 15:50:59 -0600

    commit

---
+++ Review Sessions Prog 1.py
@@ -0,0 +1,24 @@
+'''
+    # ---------- Review Sessions ---------- #
+
+
+for i in range (0, 101):
+    if i % 3:
+        print("Fizz\n")
+    elif i % 5:
+        print("Buzz\n")
+            
+    elif i % 3 and i % 5:
+        print("FizzBuzz\n")
+
+
+#if x / 3, print fizz 
+# x/5 print buzz   elif
+# x/3 nad x/5 print fizzbuzz    elif
+
+
+if __name__ in "__main__":
+    
+    pass
+'''
+
